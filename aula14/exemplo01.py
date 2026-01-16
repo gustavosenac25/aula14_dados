05787adf4c83ab1c3df4a799676ef76319ac6567
gustavosenac25 <gustavo.ocosta@aluno.senac.rio>
2026-01-15 21:52:23 -0300

    exemplo 02

--- aula14/exemplo01.py
+++ aula14/exemplo01.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import os
 
 os.system('cls')
@@ -13,16 +14,39 @@ df_planilha_custos['Custo Total (R$)'] = (
 ).round(2)
 
 #print(df_planilha_custos.head())
-print(df_planilha_custos[['Produto', 'Custo Total (R$)']].head())
+# print(df_planilha_custos[['Produto', 'Custo Total (R$)']].head())
 
-total_custos = df_planilha_custos['Custo Total (R$)'].sum()
-media_custos = df_planilha_custos['Custo Total (R$)'].mean()
-menor_custos = df_planilha_custos['Custo Total (R$)'].min()
-maior_custos = df_planilha_custos['Custo Total (R$)'].max()
+# total_custos = df_planilha_custos['Custo Total (R$)'].sum()
+# media_custos = df_planilha_custos['Custo Total (R$)'].mean()
+# menor_custos = df_planilha_custos['Custo Total (R$)'].min()
+# maior_custos = df_planilha_custos['Custo Total (R$)'].max()
 
-print(40*"=")
-print('\nResumo: (R$)')
-print(f'Total: {total_custos}')
-print(f'Média de Custo: {media_custos}')
-print(f'Menor Custo: {menor_custos}')
-print(f'Maior Custo: {maior_custos}')
+# print(40*"=")
+# print('\nResumo: (R$)')
+# print(f'Total: {total_custos}')
+# print(f'Média de Custo: {media_custos}')
+# print(f'Menor Custo: {menor_custos}')
+# print(f'Maior Custo: {maior_custos}')
+
+#---------------- Medidas estatísticas - Biblioteca Numpy
+array_custo_total = np.array(df_planilha_custos['Custo Total (R$)']) #array transforma em uma lista os números da tabela
+# print(array_custo_total)
+
+media = np.mean(array_custo_total)
+mediana = np.median(array_custo_total) #50% dos produtos têm um custo menor que isso, 50% dos produtos têm um custo maior que esse
+
+#Calculando Quartis
+q1 = np.quantile(array_custo_total, 0.25) #25% dos produtos tem um custo de até isso e 75%$ custam mais que isso
+q2 = np.quantile(array_custo_total, 0.50) #50% dos produtos tem um custo de até isso e 50%$ custam mais que isso
+q3 = np.quantile(array_custo_total, 0.75) #75% dos produtos tem um custo de até isso e 25%$ custam mais que isso
+
+df_maiores = df_planilha_custos[df_planilha_custos ['Custo Total (R$)'] > q3]
+
+print(40*'-')
+print('\nMedidas Estatísticas:')
+print(f'Média: {media}')
+print(f'Mediana: {mediana}')
+print(f'Q1: {q1}')
+print(f'Q2: {q2}')
+print(f'Q3: {q3}')
+print(df_maiores)
---
+++ aula14/exemplo02.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+dados_salario = [2000, 2500, 3000, 4000, 30000]
+media = np.mean(dados_salario)
+mediana = np.median(dados_salario)
+
+print('Média:', media)
+print('Mediana', mediana)
+
+
